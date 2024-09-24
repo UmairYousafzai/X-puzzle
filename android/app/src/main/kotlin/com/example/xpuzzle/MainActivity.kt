5497d1ce55d25f2cd1d74b37665d2b7e96400c56
Muhammad Umair Ahmad Khan <91127248+UmairYousafzai@users.noreply.github.com>
2024-09-24 16:23:44 +0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/xpuzzle/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.xpuzzle
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/service/test_service.dart
@@ -0,0 +1,9 @@
+import 'dart:convert';
+
+
+class TestsService {
+  Future<List<dynamic>> fetchTests() async {
+
+    return json.decode("") as List<dynamic>;
+  }
+}
