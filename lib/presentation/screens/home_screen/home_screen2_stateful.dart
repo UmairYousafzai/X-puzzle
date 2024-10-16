ff5a8e8135261f1b64f5d86cb8f5a9e6c8365ec5
ShafiqRasool <shafiq.rasool@rapidzzsolutions.com>
2024-10-16 19:58:46 +0500

    styles list stateful screen

--- lib/presentation/screens/home_screen/home_screen2_stateful.dart
+++ lib/presentation/screens/home_screen/home_screen2_stateful.dart
@@ -1,9 +1,16 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter/material.dart';
+import '../../../data/data_source/local/question_generator.dart';
+import '../../../domain/entities/question.dart';
+import '../../../domain/use_cases/get_questions.dart';
+import '../../../domain/use_cases/store_questions.dart';
 import '../../providers/home_screen_providers.dart';
+import '../../providers/question/question_provider.dart';
+import '../../providers/question/question_usecase_provider.dart';
+import '../../theme/colors.dart';
+import '../../widgets/snackBar_messages.dart';
 import '../game_screen/game_screen.dart';
 import 'home_screen2_card_design.dart';
-import 'home_screen_card_design.dart';
 
 class HomeScreen2ConsumerWidget extends ConsumerStatefulWidget {
   const HomeScreen2ConsumerWidget({super.key});
@@ -16,20 +23,143 @@ class HomeScreen2ConsumerWidget extends ConsumerStatefulWidget {
 
 class HomeScreen2ConsumerState extends ConsumerState<HomeScreen2ConsumerWidget> {
 
+  Future<void> generateAndStoreQuestion({
+    required GetQuestions getQuestionsUseCase,
+    required StoreQuestions storeQuestions,
+    required QuestionProviderNotifier questionNotifier,
+    bool isPPAndPS = false,
+    bool isPPAndNS = false,
+    bool isNPAndPS = false,
+    bool isNPAndNS = false,
+  }) async {
+    List<Question> questions = generatePositiveMultipleAndPositiveInteger(
+        numberOfQuestion: 10,
+        isPPAndPS: isPPAndPS,
+        isPPAndNS: isPPAndNS,
+        isNPAndPS: isNPAndPS,
+        isNPAndNS: isNPAndNS);
+
+    await storeQuestions.execute(questions);
+    if (questions.isNotEmpty) {
+      await getQuestions(getQuestionsUseCase, questionNotifier,
+          isPPAndPS: isPPAndPS,
+          isPPAndNS: isPPAndNS,
+          isNPAndPS: isNPAndPS,
+          isNPAndNS: isNPAndNS);
+    }
+  }
+
+  Future<List<Question>> getQuestions(
+      GetQuestions getQuestion,
+      QuestionProviderNotifier questionNotifier, {
+        bool isPPAndPS = false,
+        bool isPPAndNS = false,
+        bool isNPAndPS = false,
+        bool isNPAndNS = false,
+      }) async {
+    var questions = await getQuestion.execute(
+        isPPAndPS: isPPAndPS,
+        isPPAndNS: isPPAndNS,
+        isNPAndPS: isNPAndPS,
+        isNPAndNS: isNPAndNS);
+
+    if (questions.isNotEmpty) {
+      questionNotifier.addQuestion(
+          questions: questions,
+          isPPAndPS: isPPAndPS,
+          isPPAndNS: isPPAndNS,
+          isNPAndPS: isNPAndPS,
+          isNPAndNS: isNPAndNS);
+    }
+
+    await Future.delayed(const Duration(milliseconds: 1000));
+    return questions;
+  }
+
+  bool isQuestionsCompleted(List<Question> questions) {
+    if (questions.isNotEmpty) {
+      for (final question in questions) {
+        if (!question.isComplete) {
+          return false;
+        }
+      }
+    } else {
+      return false;
+    }
+    return true;
+  }
+
   @override
   Widget build(BuildContext context) {
     final cards=ref.watch(homeScreenStylesProvider);
 
-    return SizedBox(
+    final screenState = ref.watch(homeViewTypeProvider);
+    final screenStateNotifier = ref.watch(homeViewTypeProvider.notifier);
+    final storeQuestionsUseCase = ref.watch(storeQuestionUseCaseProvider);
+    final isPPAndPSQuestionUseCase = ref.watch(isPPAndPSUseCaseProvider);
+    final isPPAndNSQuestionUseCase = ref.watch(isPPAndNSUseCaseProvider);
+    final isNPAndPSQuestionUseCase = ref.watch(isNPAndPSUseCaseProvider);
+    final isNPAndNSQuestionUseCase = ref.watch(isNPAndPSUseCaseProvider);
+    final getQuestionUseCase = ref.watch(getQuestionUseCaseProvider);
+    var questionNotifier = ref.read(questionProvider.notifier);
+    final navigator = Navigator.of(context);
+    final ctxt= context;
+    return screenState["is_loading"]
+        ? Center(
+      child: CircularProgressIndicator(
+        color: MColors().colorSecondaryBlueLight,
+      ),
+    )
+        :
+
+     SizedBox(
       width: MediaQuery.of(context).size.width,
       height: MediaQuery.of(context).size.height * 0.715,
       child: ListView.builder(
         itemCount: cards.length,
         itemBuilder: (BuildContext context, int index) {
           return InkWell(
-            onTap: (){
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (ctx) =>  const GameScreen()));
+            onTap: ()async{
+              screenStateNotifier.setLoading(true);
+              var questions = await getQuestions(
+                  getQuestionUseCase, questionNotifier,
+                  isPPAndPS: index == 0,
+                  isPPAndNS: index == 1,
+                  isNPAndPS: index == 2,
+                  isNPAndNS: index == 3);
+              var isQuestionsExist = false;
+              if (index == 0) {
+                isQuestionsExist =
+                    await isPPAndPSQuestionUseCase.execute();
+              } else if (index == 1) {
+                isQuestionsExist =
+                    await isPPAndNSQuestionUseCase.execute();
+              } else if (index == 2) {
+                isQuestionsExist =
+                    await isNPAndPSQuestionUseCase.execute();
+              } else if (index == 3) {
+                isQuestionsExist =
+                    await isNPAndNSQuestionUseCase.execute();
+              }
+              if (isQuestionsExist && questions.isEmpty) {
+                screenStateNotifier.setLoading(false);
+                showErrorSnackBar(
+                    ctxt, "This style already attempted.");
+              } else {
+                if (questions.isEmpty) {
+                  await generateAndStoreQuestion(
+                  getQuestionsUseCase: getQuestionUseCase,
+                  storeQuestions: storeQuestionsUseCase,
+                  questionNotifier: questionNotifier,
+                  isPPAndPS: index == 0,
+                  isPPAndNS: index == 1,
+                  isNPAndPS: index == 2,
+                  isNPAndNS: index == 3);
+                }
+                screenStateNotifier.setLoading(false);
+                navigator.push(MaterialPageRoute(
+                    builder: (ctx) => const GameScreen()));
+              }
             },
             child: Padding(
               padding: const EdgeInsets.symmetric(vertical: 8.0),
