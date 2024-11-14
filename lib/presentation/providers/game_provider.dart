14cec6d70d94fb25b8c2e3539f2758ba6a914ad0
ShafiqRasool <shafiq.rasool@rapidzzsolutions.com>
2024-11-14 14:33:44 +0500

    UI fixing: main game screen.

--- lib/presentation/providers/game_provider.dart
+++ lib/presentation/providers/game_provider.dart
@@ -115,8 +115,7 @@ class GameNotifier extends StateNotifier<GameState> {
   }
 
   void updateQuestion(Question question) {
-    state =
-        state.copyWith(question: question, firstNumber: "", secondNumber: "");
+    state = state.copyWith(question: question, firstNumber: "", secondNumber: "");
     // state.copyWith(question: question);
   }
 
